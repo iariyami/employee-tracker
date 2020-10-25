1ea8e205718e62699978134bc379000b9aa4de57
Ibrahim Riyami <iariyami@ncsu.edu>
2020-10-25 11:43:15 -0400

    added code snippet from activity 13 week 12

--- employeeTracker.js
+++ employeeTracker.js
@@ -66,3 +66,57 @@ function runSearch() {
       }
     });
 }
+// function artistSearch() {
+//   inquirer
+//     .prompt({
+//       name: "artist",
+//       type: "input",
+//       message: "What artist would you like to search for?"
+//     })
+//     .then(function(answer) {
+//       var query = "SELECT position, song, year FROM top5000 WHERE ?";
+//       connection.query(query, { artist: answer.artist }, function(err, res) {
+//         for (var i = 0; i < res.length; i++) {
+//           console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
+//         }
+//         runSearch();
+//       });
+//     });
+// }
+
+// function multiSearch() {
+//   var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
+//   connection.query(query, function(err, res) {
+//     for (var i = 0; i < res.length; i++) {
+//       console.log(res[i].artist);
+//     }
+//     runSearch();
+//   });
+// }
+
+// function rangeSearch() {
+//   inquirer
+//     .prompt([
+//       {
+//         name: "start",
+//         type: "input",
+//         message: "Enter starting position: ",
+//         validate: function(value) {
+//           if (isNaN(value) === false) {
+//             return true;
+//           }
+//           return false;
+//         }
+//       },
+//       {
+//         name: "end",
+//         type: "input",
+//         message: "Enter ending position: ",
+//         validate: function(value) {
+//           if (isNaN(value) === false) {
+//             return true;
+//           }
+//           return false;
+//         }
+//       }
+//     ])
